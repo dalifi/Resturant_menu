cd9dcf0c3581edaee3eca31e991e4db3806e776e
Namchheav009 <dalifiken@gmail.com>
2026-01-31 15:10:49 +0700

    update some ui
    update some ui

--- obj/Debug/net10.0/Resturant_Menu.AssemblyInfo.cs
+++ obj/Debug/net10.0/Resturant_Menu.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resturant_Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98e3089080d2075cc1a9fefff1e4b2676c00809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0eb81f4adf95760aa3438f50cba08e55cee758")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resturant_Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resturant_Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
